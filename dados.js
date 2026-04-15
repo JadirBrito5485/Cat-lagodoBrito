695b155cfeaefdb61337e6f30925562680d36622
JadirBrito5485 <brito302022@gmail.com>
2026-04-15 17:02:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,15 @@
+// ARQUIVO DE DADOS - Rio Negrinho Shop
+const products = [
+    { id: 1, nome: "Mochila Militar Tática Impermeável 50 Litros Grande Reforçada com Brinde Bandeira1", preco: 139.00, desc: "FRETE GRÁTIS - CHEGA AMANHÃ", img: "https://down-br.img.susercontent.com/file/br-11134207-7r98o-m0yvwkfatbmm3e@resize_w900_nl.webp" },
+    { id: 2, nome: "Fone De Ouvido Sem Fio E6s Bluetooth 5.0 Tws Com Tela LED Digital", preco: 44.00, desc: "FRETE GRÁTIS - CHEGA AMANHÃ", img: "https://down-br.img.susercontent.com/file/br-11134211-7qukw-lixj4075llbped.webp" },
+    { id: 3, nome: "Balança Digital Medidora Corporal Bioimpedância até 180kg via Bluethooth", preco: 69.00, desc: "FRETE GRÁTIS - CHEGA AMANHÃ", img: "https://down-br.img.susercontent.com/file/sg-11134201-7rdwg-mdq4q84uj2ck74@resize_w900_nl.webp" }
+]; // <-- O ERRO ESTAVA AQUI (Faltava fechar o colchete e o ponto e vírgula)
+
+const versiculos = [
+    { texto: "O Senhor é o meu pastor, nada me faltará. Deitar-me faz em verdes pastos, guia-me mansamente a águas tranquilas.", ref: "Salmos 23:1-2" },
+    { texto: "Posso todas as coisas naquele que me fortalece.", ref: "Filipenses 4:13" },
+    { texto: "O Senhor te abençoe e te guarde; o Senhor faça resplandecer o seu rosto sobre ti e tenha misericórdia de ti.", ref: "Números 6:24-25" },
+    { texto: "Deus é o nosso refúgio e fortaleza, socorro bem presente na angústia.", ref: "Salmos 46:1" },
+    { texto: "A alegria do Senhor é a vossa força.", ref: "Neemias 8:10" },
+    { texto: "Não fui eu que lhe ordenei? Seja forte e corajoso! Não se apavore nem desanime, pois o Senhor, o seu Deus, estará com você por onde você andar.", ref: "Josué 1:9" }
+];
